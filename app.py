848419332c9b62766567197e7e1af0c907086051
ButterWizardTRUE <166186715+ButterWizardTRUE@users.noreply.github.com>
2025-04-02 10:46:14 -0700

    Corrigiendo ruta del CSV para compatibilidad en Render

--- app.py
+++ app.py
@@ -3,21 +3,17 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 
-# Obtiene la ruta del archivo en el mismo directorio que app.py
 file_path = os.path.join(os.path.dirname(__file__), "vehicles_us.csv")
 
-# Cargar los datos
 car_data = pd.read_csv(file_path, encoding="utf-8")
 
 st.title("Análisis de Datos de Vehículos")
 
-# Histograma del odómetro
 if st.checkbox("Mostrar Histograma"):
     fig_hist = px.histogram(car_data, x="odometer",
                             title="Histograma del Odómetro")
     st.plotly_chart(fig_hist)
 
-# Gráfico de dispersión: odómetro vs precio
 if st.checkbox("Mostrar Gráfico de Dispersión"):
     fig_scatter = px.scatter(car_data, x="odometer", y="price",
                              title="Relación entre Kilometraje y Precio")
